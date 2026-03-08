864b66c43593ca0c0c53d512ccb6b6b79df8aa15
imlnr <imlnr2002@gmail.com>
2026-03-08 19:51:25 +0530

    final commit

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -13,6 +13,7 @@ api.interceptors.request.use((config) => {
     if (token && config.headers) {
         config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
 }, (error) => {
     return Promise.reject(error);
